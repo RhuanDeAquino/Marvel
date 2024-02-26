668a0cf206e4e8e65553fac30ae641f43766d648
Rhuan <rhuandeaquino19@gmail.com>
2024-02-26 15:41:58 -0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-function App() {
+const Content = styled.section`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #ff6600;
+`
+
+const BoxContent = styled.div`
+  text-align: center;
+  max-width: 900px;
+  width: 80%;
+  height: 40vh;
+  background: #fff;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 2rem;
+  border-radius: 12px;
+  border: 3px solid black;
+
+  >img {
+    width: 200px;
+    position: absolute;
+    top: -5rem;
+    right: 0;
+  }
+`
+
+const Title = styled.h1`
+  font-size: 2.2rem;
+  margin-top: -4rem;
+`
+
+const Text = styled.p`
+  font-weight: 600;
+  height: 3rem;
+`
+
+const Button = styled.a`
+  background: red;
+  position: absolute;
+  bottom: -7%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  padding: 1rem 0;
+  border-radius: 12px;
+  text-decoration: none;
+  color: #fff;  
+  cursor: pointer;
+  border: 3px solid black;
+`
+
+const ChuckNorrisJoke = () => {
+  const [joke, setJoke] = useState('');
+
+  const fetchJoke = async () => {
+    try {
+      const response = await axios.get('https://api.chucknorris.io/jokes/random');
+      setJoke(response.data.value);
+    } catch (error) {
+      console.error('Error fetching Chuck Norris joke:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <BoxContent>
+        <img src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" alt='' />
+        <Title>Frases do Chuck Norris</Title>
+        <Text>{joke}</Text>
+        <Button onClick={fetchJoke}>Gerar</Button>
+      </BoxContent>
+    </Content>
   );
-}
+};
 
-export default App;
+export default ChuckNorrisJoke;
